f7783699f978a3307e860f51b5ee7b0fc8aeb2b0
utkarsh <utkjaiswal58@gmail.com>
2023-12-13 02:48:04 +0530

    login and sign up api added

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,18 @@
 "use client"
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function SignupPage() {
+
+    const router = useRouter()
+
+    const [buttonDisabled, setButtonDisabled] = React.useState(false)
+
+    const [loading, setLoading] = React.useState(false)
+
     const [user,setUser] = React.useState({
         name:"",
         email:"",
@@ -14,10 +22,39 @@ export default function SignupPage() {
 
     const onSignUp = async () => {
 
+        try {
+
+            setLoading(true)
+
+            const response = await axios.post("/api/users/signup",user)
+
+            console.log("Signup success",response.data)
+
+            router.push("/login")
+            
+        } catch (error:any) {
+            console.log("Sign Up Failed",error.message)
+            toast.error(error.message)
+            
+        } finally {
+            setLoading(false)
+        }
+
     }
+
+    useEffect (()=>{
+        if(user.email.length > 0 && user.password.length>0 && user.name.length>0 && user.phone.length==10){
+            setButtonDisabled(false)
+        }
+
+        else{
+            setButtonDisabled(true)
+        }
+
+    },[user])
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Signup</h1>
+            <h1>{ loading?"Processing":"SignUp"}</h1>
             <hr />
             <label htmlFor="email">Email</label>
             <input 
@@ -62,7 +99,7 @@ export default function SignupPage() {
             <button
              onClick={onSignUp}
              className="p-2 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
-                SignUp
+                {buttonDisabled?"No Sign Up":"Sign Up"}
             </button>
 
             <Link href="/login">Visit Login Page</Link>
---
+++ src/models/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name"]
+    },
+
+    email:{
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true
+    },
+
+    password:{
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+
+    isVerified:{
+        type: Boolean,
+        default: false,
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default:false
+    },
+
+    verifyToken:String,
+
+    verifyTokenExpiry: Date,
+
+})
+
+const User = mongoose.models.users || mongoose.model("users",userSchema)
+
+export default User
