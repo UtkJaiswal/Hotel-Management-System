5f475d133b5a8ce2a7b1401d4efb56715b5cf285
utkarsh <utkjaiswal58@gmail.com>
2023-12-13 03:18:08 +0530

    logout functionality added

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -1,9 +1,40 @@
+"use client"
+
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
 export default function RoomsPage() {
+
+    const router = useRouter()
+
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout")
+            toast.success("Logout successful")
+            router.push("/login")
+            
+        } catch (error:any) {
+            console.log("Error",error.message)
+            toast.error(error.message)
+            
+        }
+
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Hotel</h1>
             <hr />
             <p>Hotel Page</p>
+
+            <hr />
+            <button
+            onClick = {logout}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Logout
+            </button>
         </div>
     )
 }
